3f1503fff85e9a429024d301e66831848c49c0a2
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-20 12:28:40 -0700

    begin movies API routes

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var upload = multer();
+
 var app = express();
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 
-var things = require("./things.js");
-var stuff = require("./stuff.js");
+//Require the Router we defined in movies.js
+var movies = require("./movies.js");
 
-//Middleware function to log request protocol
-// app.use("/things", function (req, res, next) {
-//   console.log("A request for things received at " + Date.now());
-//   next();
-// });
-
-// home directory
-// .all similar to rails resources
-// var personSchema = mongoose.Schema({
-//   name: String,
-//   age: Number,
-//   nationality: String,
-// });
-// var Person = mongoose.model("Person", personSchema);
-
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-
-app.get("/person", function (req, res) {
-  res.render("person");
-});
-// app.get("/first_template", function (req, res) {
-//   res.render("first_view");
-// });
-
-// app.get("/dynamic_view", function (req, res) {
-//   res.render("dynamic", {
-//     name: "Hayden's Dynamic Page",
-//     url: "http://google.com",
-//   });
-// });
-
-// pages
-app.use("/things", things);
-app.use("/stuff", stuff);
+//Use the Router on the sub route /movies
+app.use("/movies", movies);
 
 app.listen(3000);

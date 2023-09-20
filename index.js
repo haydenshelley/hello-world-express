aae0c8ed604f5bdd6364d619358437fac2a47ee6
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-20 10:59:09 -0700

    add bad route handler

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ var things = require("./things.js");
 var stuff = require("./stuff.js");
 
 // home directory
-app.get("/", function (req, res) {
+// .all similar to rails resources
+app.all("/", function (req, res) {
   res.send("Hello World!");
 });
 
 // pages
 app.use("/things", things);
 app.use("/stuff", stuff);
+
+//handle bad requests
+app.get("*", function (req, res) {
+  res.send("Sorry, this is an invalid URL.");
+});
 
 app.listen(3000);
--- things.js
+++ things.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
+//get
 router.get("/", function (req, res) {
   res.send("GET route on things.");
 });
+//post
 router.post("/", function (req, res) {
   res.send("POST route on things.");
+});
+//show
+router.get("/:id", function (req, res) {
+  res.send("id: " + req.params.id);
 });
 
 //export this router to use in our index.js

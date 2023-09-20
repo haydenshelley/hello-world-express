314e28816b31053dc1f1d5068f07c769bc25da8c
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-20 11:48:04 -0700

    add templates

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./views");
+
 var things = require("./things.js");
 var stuff = require("./stuff.js");
 
+//Middleware function to log request protocol
+app.use("/things", function (req, res, next) {
+  console.log("A request for things received at " + Date.now());
+  next();
+});
+
 // home directory
 // .all similar to rails resources
-app.all("/", function (req, res) {
+app.get("/", function (req, res) {
   res.send("Hello World!");
+});
+
+app.get("/first_template", function (req, res) {
+  res.render("first_view");
+});
+
+app.get("/dynamic_view", function (req, res) {
+  res.render("dynamic", {
+    name: "Hayden's Dynamic Page",
+    url: "http://google.com",
+  });
 });
 
 // pages
 app.use("/things", things);
 app.use("/stuff", stuff);
-
-//handle bad requests
-app.get("*", function (req, res) {
-  res.send("Sorry, this is an invalid URL.");
-});
 
 app.listen(3000);

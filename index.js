4ba9a6c4dad4935b56fcd8149ab7adc71d229176
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-20 12:10:52 -0700

    new person view

--- index.js
+++ index.js
@@ -8,27 +8,37 @@ var things = require("./things.js");
 var stuff = require("./stuff.js");
 
 //Middleware function to log request protocol
-app.use("/things", function (req, res, next) {
-  console.log("A request for things received at " + Date.now());
-  next();
-});
+// app.use("/things", function (req, res, next) {
+//   console.log("A request for things received at " + Date.now());
+//   next();
+// });
 
 // home directory
 // .all similar to rails resources
+// var personSchema = mongoose.Schema({
+//   name: String,
+//   age: Number,
+//   nationality: String,
+// });
+// var Person = mongoose.model("Person", personSchema);
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.get("/first_template", function (req, res) {
-  res.render("first_view");
+app.get("/person", function (req, res) {
+  res.render("person");
 });
+// app.get("/first_template", function (req, res) {
+//   res.render("first_view");
+// });
 
-app.get("/dynamic_view", function (req, res) {
-  res.render("dynamic", {
-    name: "Hayden's Dynamic Page",
-    url: "http://google.com",
-  });
-});
+// app.get("/dynamic_view", function (req, res) {
+//   res.render("dynamic", {
+//     name: "Hayden's Dynamic Page",
+//     url: "http://google.com",
+//   });
+// });
 
 // pages
 app.use("/things", things);
